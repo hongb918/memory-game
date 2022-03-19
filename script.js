032e05ae0c539cc9739bd41486c454815eca63bf
hongb918 <hong.b918@gmail.com>
2022-03-18 22:44:58 -0500

    completing js

--- script.js
+++ script.js
@@ -8,94 +8,123 @@ const moveDisplay = document.querySelector('.moves');
 const scoreDisplay = document.querySelectorAll('.points')
 
 
-const game = document.getElementsByClassName('.board');
-const grid = document.getElementsByClassName('.grid');
-let cards = document.querySelectorAll('card');
+
 
 const cardIndex = [
     {
         name: 'blue',
-        img: 'lib/blue.png'
+        img: './blue.png'
     },
     {
         name: 'nose',
-        img: 'lib/nose.png'
+        img: './nose.png'
     },
     {
         name: 'orange',
-        img: 'lib/orange.png'
+        img: './orange.png'
     },
     {
         name: 'rocket',
-        img: 'lib/rcoket.png'
+        img: './rocket.png'
     },
     {
         name: 'tv',
-        img: 'lib/tv.png'
+        img: './tv.png'
     },
     {
         name: 'playful',
-        img: 'lib/playful.png'
+        img: './playful.png'
     }
 ]
 
 //sorting the array
-cardIndex.sort(() => 0.5 - Math.randon())
+const gameBoard = cardIndex.concat(cardIndex);
+gameBoard.sort(() => {
+    return 0.5 - Math.random();
+})
 
-function setBoard() {
-    for (let i = 0; i < cardIndex.length; i++) {
-        //setting cards attribute to data set of i 
-        cards.dataset.name = cardIndex[i].name;
+const game = document.getElementById('board');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
 
-        let front = document.createElement('div');
-        front.classList.add('front');
 
-        //declaring the bacground image of the back of the card using the object value of images    
-        let back = document.createElement('div');
-        back.style.backgroundImage = `url(${cardIndex[i].img})`;
+for (let i = 0; i < gameBoard.length; i++) {
+    let cards = document.createElement('div');
+    cards.classList.add('card');
+    //setting cards attribute to data set of i 
+    cards.dataset.name = gameBoard[i].name;
 
-        cards.appendChild(front);
-        cards.appendChild(back);
-    }
+    let front = document.createElement('div');
+    front.classList.add('front');
+
+    //declaring the bacground image of the back of the card using the object value of images    
+    let back = document.createElement('div');
+    back.style.backgroundImage = `url(${gameBoard[i].img})`;
+
+    grid.appendChild(cards);
+    cards.appendChild(front);
+    cards.appendChild(back);
 }
+
 
 let firstCard = '';
 let secondCard = '';
-
+let moves = 0;
 let previousTarget = null;
 let delay = 1200;
 
 
-// function matchingSet() {
+let matchingPair = function () {
+    let chosen = document.querySelectorAll('.chosen');
+    for (let i = 0; i < chosen.length; i++) {
+        chosen[i].classList.add('match')
+    }
+}
 
-// }
-
-
-
-
-//Wining Message
-const winningMessage = () => `You've won!`;
-
-
-// const cellIndex = Array.from(parseInt(cards.getAttribute('data-cell-index')));
-
-// cellIndex.sort(() => 0.5 - Math.random())
-
-
-// const gameGrid = cellIndex.concat(cellIndex);
-// gameGrid.sort(() => {
-//     return 0.5 - Math.random();
-// })
-
-
-
-function restartGame() {
+const restartGame = () => {
     firstCard = '';
     secondCard = '';
     moves = 0;
     previousTarget = null;
 
-}
+    let chosen = document.querySelectorAll('.chosen');
+    for (let i = 0; i < chosen.length; i++) {
+        chosen[i].classList.remove('chosen');
+    }
+};
+
+
+grid.addEventListener('click', (event) => {
+    let clickedCard = event.target;
+    if (clickedCard.nodeName === 'section' || clickedCard === previousTarget || clickedCard.parentNode.classList.contains('chosen')) {
+        return;
+    }
+    if (moves < 2) {
+        moves++;
+        if (moves === 1) {
+            firstCard = clickedCard.parentNode.dataset.name;
+            clickedCard.parentNode.classList.add('chosen');
+        } else {
+            secondCard = clickedCard.parentNode.dataset.name;
+            clickedCard.parentNode.classList.add('chosen');
+        }
+        if (firstCard !== '' && secondCard !== '') {
+            if (firstCard === secondCard) {
+                setTimeout(matchingPair, delay);
+                setTimeout(restartGame, delay)
+            } else {
+                setTimeout(restartGame, delay)
+            }
+        }
+        previousTarget = clickedCard;
+    }
+})
+
+
+
+//Wining Message
+const winningMessage = () => `You've won!`;
 
 // function showingMoves () {
 
@@ -110,8 +139,6 @@ function restartGame() {
 
 
 //setting event listeners for game card
-document.querySelectorAll('.card').forEach(cards => addEventListener('click', cellClick));
 
 
 //setting event listners for reset button
-document.querySelector('.reset').addEventListener('click', restartGame);
